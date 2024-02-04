a469beca9a59add4ef952eab0316d36a83392a32
Deena Athithyaa D <deenaathithyaa11@gmail.com>
2024-02-04 17:57:13 +0530

    adding-files

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "Creative Thinker", "Problem Solver", "Tech Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  fadeOut: true,
+});
+var typed = new Typed(".typing-2", {
+  strings: ["Web Developer", "Creative Thinker", "Problem Solver", "Tech Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  fadeOut: true,
+});
